8404dc8a70491752fd7cd20396479f568cadb445
ㅇㅇㅇㅇ <milkymarky@naver.com>
2024-09-11 13:42:05 +0900

    ㅇㅇ

--- src/main/java/com/nhnacademy/thread/CounterHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterHandler.java
@@ -15,30 +15,37 @@ package com.nhnacademy.thread;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class CounterHandler implements Runnable  {
+public class CounterHandler implements Runnable {
     private final long countMaxSize;
-
     private long count;
 
     public CounterHandler(long countMaxSize) {
-        if(countMaxSize<=0){
-            throw new IllegalArgumentException();
+        // TODO#1 countMaxSize가 0 이하인 경우 IllegalArgumentException 예외를 발생시킵니다.
+        if (countMaxSize <= 0) {
+            throw new IllegalArgumentException("countMaxSize must be greater than 0");
         }
 
+        // TODO#2 countMaxSize 값을 초기화하고 count를 0으로 초기화합니다.
         this.countMaxSize = countMaxSize;
-        this.count=0l;
+        this.count = 0;
     }
 
     @Override
     public void run() {
         do {
             try {
+                // TODO#3 1초 간격으로 count 값을 증가시킵니다.
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                // TODO#4 InterruptedException을 RuntimeException으로 감싸서 발생시킵니다.
+                Thread.currentThread().interrupt(); // 스레드의 인터럽트 상태를 복원합니다.
+                log.error("Thread was interrupted", e);
+                return; // 인터럽트가 발생하면 스레드를 종료합니다.
             }
+
             count++;
-            log.debug("thread:{},count:{}",Thread.currentThread().getName(),count);
-        }while (count<countMaxSize);
+            // TODO#5 현재 스레드의 이름과 count 값을 로그로 출력합니다.
+            log.debug("thread:{}, count:{}", Thread.currentThread().getName(), count);
+        } while (count < countMaxSize);
     }
 }
